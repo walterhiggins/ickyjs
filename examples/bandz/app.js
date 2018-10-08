271f849e6bd6d2c93ff5dca22e968435c209b08a
Walter Higgins <walter.higgins@gmail.com>
2018-10-08 23:20:09 +0100

    1st draft of bandz app

---
+++ examples/bandz/app.js
@@ -0,0 +1,26 @@
+//fetch("https://musicbrainz.org/ws/2/artist?query=The%20Stone%20Roses&fmt=json")
+//    .then(response => response.json())
+//    .then(
+let { fname, update } = icky;
+
+const tSearch = (placeholder, callback) => `
+  <span class="search">
+    <input placeholder="${placeholder}"
+           onchange="${fname(callback)}(this.value)">
+    <i class="fas fa-search"></i>
+  </span>`;
+
+function searchByBandname(bandName) {
+  console.log("Band: " + bandName);
+}
+function searchBySong(bandName) {
+  console.log("Song: " + bandName);
+}
+update(
+  "#root",
+  () => `
+  <h1><i class="fas fa-headphones"></i>Bandz</h1>
+  <p>${tSearch("Search Band", searchByBandname)}</p>
+  <p>${tSearch("Search Song", searchBySong)}</p>
+  `
+);
