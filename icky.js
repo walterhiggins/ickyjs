e5171ca2e72549c3c59e6d8ea0e3ceefea6bb42f
Walter Higgins <walterh4@ie.ibm.com>
2018-10-11 17:00:24 +0100

    Adding comments for icky.js

--- icky.js
+++ icky.js
@@ -1,58 +1,114 @@
-// ------------------------------------------------------------------------
-// icky.js is a tiny ES6 library to make writing simple web apps easier
-// ------------------------------------------------------------------------
-
+// icky.js
+// =======
+//
+// icky.js is a tiny ES6 library to make writing _simple_ web apps easier.
+//
+// Its goal is to make writing small prototype webapps easy and fun. The premise of this library
+// is based on a couple of assumptions:
+//
+// * DOM manipulation in Javascript using the DOM API is hard.
+// * On the other hand String construction using ES6 Template Literals is easy.
+// * Assigning a string to an element's `.innerHTML` property will not open the gates to hell ;-)
+//
+// If you disagree with any of these assumptions (especially the last one) you should probably not
+// use this library.
+//
 (function(exports) {
   "use strict";
-  //
-  // given a function assign it a globally-accessible name
-  // so it can be used as an event handler.
-  //
-  let icky = {};
-  (function(exports) {
-    const DEFAULT_NAMESPACE = "functions";
-    var id = 0;
-    exports.namespaces = {};
-    exports.namespaces[DEFAULT_NAMESPACE] = {};
-    function _globallyNameFunction(namespace, fn) {
-      var name = `fn_${id++}`;
-      if (exports.namespaces[namespace]) {
-        exports.namespaces[namespace][name] = fn;
-        return `icky.namespaces.${namespace}.${name}`;
-      } else {
-        let errMsg = `icky.js error: namespace '${namespace}' does not exist`;
-        console.error(errMsg);
-        throw new Error(errMsg);
-      }
+  /* default namespace used for functions is `window.icky.namespaces.functions` */
+  const DEFAULT_NAMESPACE = "functions";
+  /* each function named via `gnf()` is assigned a unique incremented name */
+  let id = 0;
+  /* initialise icky */
+  let icky = {
+    namespaces: {
+      [DEFAULT_NAMESPACE]: {}
     }
-    function globallyNameFunction(namespaceOrFunction) {
-      if (namespaceOrFunction.constructor == Function) {
-        return _globallyNameFunction(DEFAULT_NAMESPACE, namespaceOrFunction);
-      } else {
-        let namespace = namespaceOrFunction;
-        // initialise namespace (freeing up memory)
-        exports.namespaces[namespace] = {};
-        return function(fn) {
-          return _globallyNameFunction(namespace, fn);
-        };
-      }
+  };
+  /* globallyNameFunction gives the function a name and keeps a reference to it on window.icky */
+  const globallyNameFunction = (namespace, fn) => {
+    var name = `fn_${id++}`;
+    if (icky.namespaces[namespace]) {
+      icky.namespaces[namespace][name] = fn;
+      return `icky.namespaces.${namespace}.${name}`;
+    } else {
+      let errMsg = `icky.js error: namespace '${namespace}' does not exist`;
+      console.error(errMsg);
+      throw new Error(errMsg);
     }
-    exports.gnf = globallyNameFunction;
-  })(icky);
+  };
+  // icky.gnf()
+  // ----------
+  // app.js
+  // ```javascript
+  // const cCustomButton = (person) => {
+  //   let myClick = icky.gnf((btn) => {
+  //     console.log(`You clicked ${person}`);
+  //   });
+  //   return `
+  //   <button onclick="${myClick}(this)">
+  //     Welcome {$person}
+  //   </button>`;
+  // };
+  // ```
+  icky.gnf = nsFn => {
+    /* parameter can be a namespace (string) or a function */
+    if (nsFn.constructor == Function) {
+      /* if it's a function then name it in the default namespace and return its name... */
+      return globallyNameFunction(DEFAULT_NAMESPACE, nsFn);
+    } else {
+      /* 
+        ...otherwise
+	1. initialise a new namespace (freeing up memory if it already exists)
+	2. return a function which can be used for naming functions in this new namespace
+      */
+      let namespace = nsFn;
+      icky.namespaces[namespace] = {};
+      return function(fn) {
+        return globallyNameFunction(namespace, fn);
+      };
+    }
+  };
 
-  //
-  // Makes use in template strings nice.
-  //
+  // icky.map()
+  // ----------
+  // app.js
+  // ```javascript
+  // ...
+  // let names = ["John","Paul","George","Ringo"];
+  // const cCustomButtonList = () => {
+  //   return `
+  //   <ol>
+  //     ${map(names, (name) => `
+  //     <li>${cCustomButton(name)}</li>
+  //     `)}
+  //   </ol>`;
+  // };
+  // ```
   icky.map = (array, fn) => array.map(fn).join("");
   //
-  // given a selector and a function set the element's
-  // contents to the function result
-  //
-  function update(view, fn) {
+  // icky.update()
+  // -------------
+  // index.html
+  // ```html
+  // <html>
+  //   <body>
+  //     <div id="peopleList"></div>
+  //     <script src="icky.js" type="text/javascript"></script>
+  //     <script src="app.js" type="text/javascript"></script>
+  //   </body>
+  // </html>
+  // ```
+  // app.js
+  // ```javascript
+  // ...
+  // icky.update("#peopleList", cCustomButtonList);
+  // ```
+  icky.update = (view, fn) => {
     var el = document.querySelector(view);
     if (el) el.innerHTML = fn(el);
     else console.error(`icky.js error: could not find ${view} element`);
-  }
-  icky.update = update;
+  };
+
   exports.icky = icky;
 })(window);
