984bd8ea327f0e465c382549490cd786bd284aba
Walter Higgins <walter.higgins@gmail.com>
2018-10-13 17:50:45 +0100

    work on bandz app

--- docs/examples/bandz/app.js
+++ docs/examples/bandz/app.js
@@ -1,7 +1,4 @@
-//fetch("https://musicbrainz.org/ws/2/artist?query=The%20Stone%20Roses&fmt=json")
-//    .then(response => response.json())
-//    .then(
-let { gnf, update } = icky;
+let { gnf, update, map } = icky;
 
 const tSearch = (placeholder, callback) => `
   <span class="search">
@@ -10,17 +7,96 @@ const tSearch = (placeholder, callback) => `
     <i class="fas fa-search"></i>
   </span>`;
 
+const EXPIRES_DAY = 60 * 60 * 24;
+
+/* nice search (don't hit musicbrainz API too frequently) */
+const ns = (url, expires) => {
+  function get() {
+    return fetch(url)
+      .then(response => response.json())
+      .then(results => {
+        const now = new Date().getTime();
+        results.fetchTime = now;
+        localStorage.setItem(url, JSON.stringify(results));
+        return results;
+      });
+  }
+  let results = localStorage.getItem(url);
+  if (!results) {
+    return get();
+  }
+  results = JSON.parse(results);
+  const now = new Date().getTime();
+  const fetched = results.fetchTime;
+  if (now - fetched > expires * 1000) {
+    // results are stale
+    return get();
+  } else {
+    return new Promise(resolve => setTimeout(() => resolve(results), 0));
+  }
+};
+
 function searchByBandname(bandName) {
-  console.log("Band: " + bandName);
+  bandName = encodeURIComponent(bandName.trim().toLowerCase());
+  ns(
+    `https://musicbrainz.org/ws/2/artist?query=${bandName}&fmt=json`,
+    EXPIRES_DAY
+  ).then(results => {
+    update("#results", () => showBandResults(results));
+  });
 }
-function searchBySong(bandName) {
-  console.log("Song: " + bandName);
+function showBandResults(results) {
+  return `
+  <ul>
+    ${map(
+      results.artists.filter(byTaggedArtist),
+      artist => `<li>${showResult(artist)}</li>`
+    )}
+  </ul>`;
 }
+function byTaggedArtist(artist) {
+  if (artist.tags) {
+    let count = 0;
+    artist.tags.forEach(tag => (count += tag.count));
+    return count > 0;
+  } else {
+    return false;
+  }
+}
+function showResult(artist) {
+  let begin = artist["life-span"] ? artist["life-span"].begin : "";
+  let end = artist["life-span"] ? artist["life-span"].end : "";
+  let beginArea = artist["begin-area"] ? artist["begin-area"].name : "";
+  let tags = [];
+  const byCount = (a, b) => {
+    return b.count - a.count;
+  };
+  if (artist.tags) {
+    tags = artist.tags;
+    tags = tags.sort(byCount).filter(tag => tag.count);
+  }
+  return `
+  <div class="artist">
+    <h2>
+    <i class="fas fa-${artist.type == "Group"
+      ? "users"
+      : "user"}"></i> ${artist.name}
+    </h2>
+    <h3>
+      ${begin} - ${end ? end : "&mdash;"} 
+      ${beginArea ? beginArea : ""}
+      ${artist.area ? ", " + artist.area.name : ""}
+  </h3>
+    ${tags.map(tag => `<span class="tag">${tag.name}</span>`)}
+  </div>
+  `;
+}
+function searchBySong(bandName) {}
 update(
   "#root",
   () => `
   <h1><i class="fas fa-headphones"></i>Bandz</h1>
   <p>${tSearch("Search Band", searchByBandname)}</p>
   <p>${tSearch("Search Song", searchBySong)}</p>
-  `
+  <div id="results"></div>`
 );
